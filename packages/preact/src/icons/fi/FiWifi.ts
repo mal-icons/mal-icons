d226f2447c53babceff1a6073b405efca646f5b3
M Anas Latif <m.anaslatif.com@gmail.com>
2026-05-20 02:44:51 +0500

    feat(preact): generate icon components (3/3)

---
+++ packages/preact/src/icons/fi/FiWifi.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiWifi = createIcon("0 0 24 24", [["path",{"d":"M5 12.55a11 11 0 0 1 14.08 0"}],["path",{"d":"M1.42 9a16 16 0 0 1 21.16 0"}],["path",{"d":"M8.53 16.11a6 6 0 0 1 6.95 0"}],["line",{"x1":"12","y1":"20","x2":"12.01","y2":"20"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
---
+++ packages/preact/src/icons/fi/FiYoutube.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiYoutube = createIcon("0 0 24 24", [["path",{"d":"M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.60.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 0.46 5.33A2.78 2.78 0 0 0 3.4 19c1.720.46 8.60.46 8.60.46s6.88 0 8.6-0.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 0.46-5.25 29 29 0 0 0-0.46-5.33z"}],["polygon",{"points":"9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
