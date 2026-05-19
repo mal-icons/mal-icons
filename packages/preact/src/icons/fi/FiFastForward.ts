66a81cb7b340f89bbfbb6b1f8f9b7abd2f6f998a
M Anas Latif <m.anaslatif.com@gmail.com>
2026-05-20 01:53:31 +0500

    feat(preact): generate icon components (2/3)

---
+++ packages/preact/src/icons/fi/FiFastForward.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiFastForward = createIcon("0 0 24 24", [["polygon",{"points":"13 19 22 12 13 5 13 19"}],["polygon",{"points":"2 19 11 12 2 5 2 19"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
---
+++ packages/preact/src/icons/fi/FiNavigation2.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiNavigation2 = createIcon("0 0 24 24", [["polygon",{"points":"12 2 19 21 12 17 5 21 12 2"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
