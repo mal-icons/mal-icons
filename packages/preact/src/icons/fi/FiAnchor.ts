18ee18f8766986a5d43f0f4eaf2a2c4dc709c46f
M Anas Latif <m.anaslatif.com@gmail.com>
2026-05-20 01:45:49 +0500

    feat(preact): generate icon components (1/3)

---
+++ packages/preact/src/icons/fi/FiAnchor.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiAnchor = createIcon("0 0 24 24", [["circle",{"cx":"12","cy":"5","r":"3"}],["line",{"x1":"12","y1":"22","x2":"12","y2":"8"}],["path",{"d":"M5 12H2a10 10 0 0 0 20 0h-3"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
---
+++ packages/preact/src/icons/fi/FiCircle.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiCircle = createIcon("0 0 24 24", [["circle",{"cx":"12","cy":"12","r":"10"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
