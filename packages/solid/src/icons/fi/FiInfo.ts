7b6eb49a1680ceb6609b5a09b60f6fe8e93afcc4
M Anas Latif <m.anaslatif.com@gmail.com>
2026-05-22 01:02:53 +0500

    feat(solid): generate icon components (2/3)

---
+++ packages/solid/src/icons/fi/FiInfo.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiInfo = createIcon("0 0 24 24", [["circle",{"cx":"12","cy":"12","r":"10"}],["line",{"x1":"12","y1":"16","x2":"12","y2":"12"}],["line",{"x1":"12","y1":"8","x2":"12.01","y2":"8"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
