6f41c1d56e6e9baf89cf23ea1ee72fd72117eba8
M Anas Latif <m.anaslatif.com@gmail.com>
2026-05-22 02:13:03 +0500

    feat(solid): generate icon components (3/3)

---
+++ packages/solid/src/icons/fi/FiTag.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiTag = createIcon("0 0 24 24", [["path",{"d":"M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"}],["line",{"x1":"7","y1":"7","x2":"7.01","y2":"7"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
