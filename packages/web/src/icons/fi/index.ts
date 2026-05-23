63e6e5a2f28f104bc5ad590a89ca1b18be6b57b6
M Anas Latif <m.anaslatif.com@gmail.com>
2026-05-24 04:55:54 +0500

    feat(web): generate icon components (3/3)

---
+++ packages/web/src/icons/fi/index.ts
@@ -0,0 +1,581 @@
+import type { IconData } from "@mal-icon/core";
+import FiActivity from "./FiActivity.json";
+import FiAirplay from "./FiAirplay.json";
+import FiAlertCircle from "./FiAlertCircle.json";
+import FiAlertOctagon from "./FiAlertOctagon.json";
+import FiAlertTriangle from "./FiAlertTriangle.json";
+import FiAlignCenter from "./FiAlignCenter.json";
+import FiAlignJustify from "./FiAlignJustify.json";
+import FiAlignLeft from "./FiAlignLeft.json";
+import FiAlignRight from "./FiAlignRight.json";
+import FiAnchor from "./FiAnchor.json";
+import FiAperture from "./FiAperture.json";
+import FiArchive from "./FiArchive.json";
+import FiArrowDown from "./FiArrowDown.json";
+import FiArrowDownCircle from "./FiArrowDownCircle.json";
+import FiArrowDownLeft from "./FiArrowDownLeft.json";
+import FiArrowDownRight from "./FiArrowDownRight.json";
+import FiArrowLeft from "./FiArrowLeft.json";
+import FiArrowLeftCircle from "./FiArrowLeftCircle.json";
+import FiArrowRight from "./FiArrowRight.json";
+import FiArrowRightCircle from "./FiArrowRightCircle.json";
+import FiArrowUp from "./FiArrowUp.json";
+import FiArrowUpCircle from "./FiArrowUpCircle.json";
+import FiArrowUpLeft from "./FiArrowUpLeft.json";
+import FiArrowUpRight from "./FiArrowUpRight.json";
+import FiAtSign from "./FiAtSign.json";
+import FiAward from "./FiAward.json";
+import FiBarChart from "./FiBarChart.json";
+import FiBarChart2 from "./FiBarChart2.json";
+import FiBattery from "./FiBattery.json";
+import FiBatteryCharging from "./FiBatteryCharging.json";
+import FiBell from "./FiBell.json";
+import FiBellOff from "./FiBellOff.json";
+import FiBluetooth from "./FiBluetooth.json";
+import FiBold from "./FiBold.json";
+import FiBook from "./FiBook.json";
+import FiBookmark from "./FiBookmark.json";
+import FiBookOpen from "./FiBookOpen.json";
+import FiBox from "./FiBox.json";
+import FiBriefcase from "./FiBriefcase.json";
+import FiCalendar from "./FiCalendar.json";
+import FiCamera from "./FiCamera.json";
+import FiCameraOff from "./FiCameraOff.json";
+import FiCast from "./FiCast.json";
+import FiCheck from "./FiCheck.json";
+import FiCheckCircle from "./FiCheckCircle.json";
+import FiCheckSquare from "./FiCheckSquare.json";
+import FiChevronDown from "./FiChevronDown.json";
+import FiChevronLeft from "./FiChevronLeft.json";
+import FiChevronRight from "./FiChevronRight.json";
+import FiChevronsDown from "./FiChevronsDown.json";
+import FiChevronsLeft from "./FiChevronsLeft.json";
+import FiChevronsRight from "./FiChevronsRight.json";
+import FiChevronsUp from "./FiChevronsUp.json";
+import FiChevronUp from "./FiChevronUp.json";
+import FiChrome from "./FiChrome.json";
+import FiCircle from "./FiCircle.json";
+import FiClipboard from "./FiClipboard.json";
+import FiClock from "./FiClock.json";
+import FiCloud from "./FiCloud.json";
+import FiCloudDrizzle from "./FiCloudDrizzle.json";
+import FiCloudLightning from "./FiCloudLightning.json";
+import FiCloudOff from "./FiCloudOff.json";
+import FiCloudRain from "./FiCloudRain.json";
+import FiCloudSnow from "./FiCloudSnow.json";
+import FiCode from "./FiCode.json";
+import FiCodepen from "./FiCodepen.json";
+import FiCodesandbox from "./FiCodesandbox.json";
+import FiCoffee from "./FiCoffee.json";
+import FiColumns from "./FiColumns.json";
+import FiCommand from "./FiCommand.json";
+import FiCompass from "./FiCompass.json";
+import FiCopy from "./FiCopy.json";
+import FiCornerDownLeft from "./FiCornerDownLeft.json";
+import FiCornerDownRight from "./FiCornerDownRight.json";
+import FiCornerLeftDown from "./FiCornerLeftDown.json";
+import FiCornerLeftUp from "./FiCornerLeftUp.json";
+import FiCornerRightDown from "./FiCornerRightDown.json";
+import FiCornerRightUp from "./FiCornerRightUp.json";
+import FiCornerUpLeft from "./FiCornerUpLeft.json";
+import FiCornerUpRight from "./FiCornerUpRight.json";
+import FiCpu from "./FiCpu.json";
+import FiCreditCard from "./FiCreditCard.json";
+import FiCrop from "./FiCrop.json";
+import FiCrosshair from "./FiCrosshair.json";
+import FiDatabase from "./FiDatabase.json";
+import FiDelete from "./FiDelete.json";
+import FiDisc from "./FiDisc.json";
+import FiDivide from "./FiDivide.json";
+import FiDivideCircle from "./FiDivideCircle.json";
+import FiDivideSquare from "./FiDivideSquare.json";
+import FiDollarSign from "./FiDollarSign.json";
+import FiDownload from "./FiDownload.json";
+import FiDownloadCloud from "./FiDownloadCloud.json";
+import FiDribbble from "./FiDribbble.json";
+import FiDroplet from "./FiDroplet.json";
+import FiEdit from "./FiEdit.json";
+import FiEdit2 from "./FiEdit2.json";
+import FiEdit3 from "./FiEdit3.json";
+import FiExternalLink from "./FiExternalLink.json";
+import FiEye from "./FiEye.json";
+import FiEyeOff from "./FiEyeOff.json";
+import FiFacebook from "./FiFacebook.json";
+import FiFastForward from "./FiFastForward.json";
+import FiFeather from "./FiFeather.json";
+import FiFigma from "./FiFigma.json";
+import FiFile from "./FiFile.json";
+import FiFileMinus from "./FiFileMinus.json";
+import FiFilePlus from "./FiFilePlus.json";
+import FiFileText from "./FiFileText.json";
+import FiFilm from "./FiFilm.json";
+import FiFilter from "./FiFilter.json";
+import FiFlag from "./FiFlag.json";
+import FiFolder from "./FiFolder.json";
+import FiFolderMinus from "./FiFolderMinus.json";
+import FiFolderPlus from "./FiFolderPlus.json";
+import FiFramer from "./FiFramer.json";
+import FiFrown from "./FiFrown.json";
+import FiGift from "./FiGift.json";
+import FiGitBranch from "./FiGitBranch.json";
+import FiGitCommit from "./FiGitCommit.json";
+import FiGithub from "./FiGithub.json";
+import FiGitlab from "./FiGitlab.json";
+import FiGitMerge from "./FiGitMerge.json";
+import FiGitPullRequest from "./FiGitPullRequest.json";
+import FiGlobe from "./FiGlobe.json";
+import FiGrid from "./FiGrid.json";
+import FiHardDrive from "./FiHardDrive.json";
+import FiHash from "./FiHash.json";
+import FiHeadphones from "./FiHeadphones.json";
+import FiHeart from "./FiHeart.json";
+import FiHelpCircle from "./FiHelpCircle.json";
+import FiHexagon from "./FiHexagon.json";
+import FiHome from "./FiHome.json";
+import FiImage from "./FiImage.json";
+import FiInbox from "./FiInbox.json";
+import FiInfo from "./FiInfo.json";
+import FiInstagram from "./FiInstagram.json";
+import FiItalic from "./FiItalic.json";
+import FiKey from "./FiKey.json";
+import FiLayers from "./FiLayers.json";
+import FiLayout from "./FiLayout.json";
+import FiLifeBuoy from "./FiLifeBuoy.json";
+import FiLink from "./FiLink.json";
+import FiLink2 from "./FiLink2.json";
+import FiLinkedin from "./FiLinkedin.json";
+import FiList from "./FiList.json";
+import FiLoader from "./FiLoader.json";
+import FiLock from "./FiLock.json";
+import FiLogIn from "./FiLogIn.json";
+import FiLogOut from "./FiLogOut.json";
+import FiMail from "./FiMail.json";
+import FiMap from "./FiMap.json";
+import FiMapPin from "./FiMapPin.json";
+import FiMaximize from "./FiMaximize.json";
+import FiMaximize2 from "./FiMaximize2.json";
+import FiMeh from "./FiMeh.json";
+import FiMenu from "./FiMenu.json";
+import FiMessageCircle from "./FiMessageCircle.json";
+import FiMessageSquare from "./FiMessageSquare.json";
+import FiMic from "./FiMic.json";
+import FiMicOff from "./FiMicOff.json";
+import FiMinimize from "./FiMinimize.json";
+import FiMinimize2 from "./FiMinimize2.json";
+import FiMinus from "./FiMinus.json";
+import FiMinusCircle from "./FiMinusCircle.json";
+import FiMinusSquare from "./FiMinusSquare.json";
+import FiMonitor from "./FiMonitor.json";
+import FiMoon from "./FiMoon.json";
+import FiMoreHorizontal from "./FiMoreHorizontal.json";
+import FiMoreVertical from "./FiMoreVertical.json";
+import FiMousePointer from "./FiMousePointer.json";
+import FiMove from "./FiMove.json";
+import FiMusic from "./FiMusic.json";
+import FiNavigation from "./FiNavigation.json";
+import FiNavigation2 from "./FiNavigation2.json";
+import FiOctagon from "./FiOctagon.json";
+import FiPackage from "./FiPackage.json";
+import FiPaperclip from "./FiPaperclip.json";
+import FiPause from "./FiPause.json";
+import FiPauseCircle from "./FiPauseCircle.json";
+import FiPenTool from "./FiPenTool.json";
+import FiPercent from "./FiPercent.json";
+import FiPhone from "./FiPhone.json";
+import FiPhoneCall from "./FiPhoneCall.json";
+import FiPhoneForwarded from "./FiPhoneForwarded.json";
+import FiPhoneIncoming from "./FiPhoneIncoming.json";
+import FiPhoneMissed from "./FiPhoneMissed.json";
+import FiPhoneOff from "./FiPhoneOff.json";
+import FiPhoneOutgoing from "./FiPhoneOutgoing.json";
+import FiPieChart from "./FiPieChart.json";
+import FiPlay from "./FiPlay.json";
+import FiPlayCircle from "./FiPlayCircle.json";
+import FiPlus from "./FiPlus.json";
+import FiPlusCircle from "./FiPlusCircle.json";
+import FiPlusSquare from "./FiPlusSquare.json";
+import FiPocket from "./FiPocket.json";
+import FiPower from "./FiPower.json";
+import FiPrinter from "./FiPrinter.json";
+import FiRadio from "./FiRadio.json";
+import FiRefreshCcw from "./FiRefreshCcw.json";
+import FiRefreshCw from "./FiRefreshCw.json";
+import FiRepeat from "./FiRepeat.json";
+import FiRewind from "./FiRewind.json";
+import FiRotateCcw from "./FiRotateCcw.json";
+import FiRotateCw from "./FiRotateCw.json";
+import FiRss from "./FiRss.json";
+import FiSave from "./FiSave.json";
+import FiScissors from "./FiScissors.json";
+import FiSearch from "./FiSearch.json";
+import FiSend from "./FiSend.json";
+import FiServer from "./FiServer.json";
+import FiSettings from "./FiSettings.json";
+import FiShare from "./FiShare.json";
+import FiShare2 from "./FiShare2.json";
+import FiShield from "./FiShield.json";
+import FiShieldOff from "./FiShieldOff.json";
+import FiShoppingBag from "./FiShoppingBag.json";
+import FiShoppingCart from "./FiShoppingCart.json";
+import FiShuffle from "./FiShuffle.json";
+import FiSidebar from "./FiSidebar.json";
+import FiSkipBack from "./FiSkipBack.json";
+import FiSkipForward from "./FiSkipForward.json";
+import FiSlack from "./FiSlack.json";
+import FiSlash from "./FiSlash.json";
+import FiSliders from "./FiSliders.json";
+import FiSmartphone from "./FiSmartphone.json";
+import FiSmile from "./FiSmile.json";
+import FiSpeaker from "./FiSpeaker.json";
+import FiSquare from "./FiSquare.json";
+import FiStar from "./FiStar.json";
+import FiStopCircle from "./FiStopCircle.json";
+import FiSun from "./FiSun.json";
+import FiSunrise from "./FiSunrise.json";
+import FiSunset from "./FiSunset.json";
+import FiTable from "./FiTable.json";
+import FiTablet from "./FiTablet.json";
+import FiTag from "./FiTag.json";
+import FiTarget from "./FiTarget.json";
+import FiTerminal from "./FiTerminal.json";
+import FiThermometer from "./FiThermometer.json";
+import FiThumbsDown from "./FiThumbsDown.json";
+import FiThumbsUp from "./FiThumbsUp.json";
+import FiToggleLeft from "./FiToggleLeft.json";
+import FiToggleRight from "./FiToggleRight.json";
+import FiTool from "./FiTool.json";
+import FiTrash from "./FiTrash.json";
+import FiTrash2 from "./FiTrash2.json";
+import FiTrello from "./FiTrello.json";
+import FiTrendingDown from "./FiTrendingDown.json";
+import FiTrendingUp from "./FiTrendingUp.json";
+import FiTriangle from "./FiTriangle.json";
+import FiTruck from "./FiTruck.json";
+import FiTv from "./FiTv.json";
+import FiTwitch from "./FiTwitch.json";
+import FiTwitter from "./FiTwitter.json";
+import FiType from "./FiType.json";
+import FiUmbrella from "./FiUmbrella.json";
+import FiUnderline from "./FiUnderline.json";
+import FiUnlock from "./FiUnlock.json";
+import FiUpload from "./FiUpload.json";
+import FiUploadCloud from "./FiUploadCloud.json";
+import FiUser from "./FiUser.json";
+import FiUserCheck from "./FiUserCheck.json";
+import FiUserMinus from "./FiUserMinus.json";
+import FiUserPlus from "./FiUserPlus.json";
+import FiUsers from "./FiUsers.json";
+import FiUserX from "./FiUserX.json";
+import FiVideo from "./FiVideo.json";
+import FiVideoOff from "./FiVideoOff.json";
+import FiVoicemail from "./FiVoicemail.json";
+import FiVolume from "./FiVolume.json";
+import FiVolume1 from "./FiVolume1.json";
+import FiVolume2 from "./FiVolume2.json";
+import FiVolumeX from "./FiVolumeX.json";
+import FiWatch from "./FiWatch.json";
+import FiWifi from "./FiWifi.json";
+import FiWifiOff from "./FiWifiOff.json";
+import FiWind from "./FiWind.json";
+import FiX from "./FiX.json";
+import FiXCircle from "./FiXCircle.json";
+import FiXOctagon from "./FiXOctagon.json";
+import FiXSquare from "./FiXSquare.json";
+import FiYoutube from "./FiYoutube.json";
+import FiZap from "./FiZap.json";
+import FiZapOff from "./FiZapOff.json";
+import FiZoomIn from "./FiZoomIn.json";
+import FiZoomOut from "./FiZoomOut.json";
+
+/** Every "fi" icon as serializable data, keyed by component name. */
+export const fiIcons = {
+  FiActivity,
+  FiAirplay,
+  FiAlertCircle,
+  FiAlertOctagon,
+  FiAlertTriangle,
+  FiAlignCenter,
+  FiAlignJustify,
+  FiAlignLeft,
+  FiAlignRight,
+  FiAnchor,
+  FiAperture,
+  FiArchive,
+  FiArrowDown,
+  FiArrowDownCircle,
+  FiArrowDownLeft,
+  FiArrowDownRight,
+  FiArrowLeft,
+  FiArrowLeftCircle,
+  FiArrowRight,
+  FiArrowRightCircle,
+  FiArrowUp,
+  FiArrowUpCircle,
+  FiArrowUpLeft,
+  FiArrowUpRight,
+  FiAtSign,
+  FiAward,
+  FiBarChart,
+  FiBarChart2,
+  FiBattery,
+  FiBatteryCharging,
+  FiBell,
+  FiBellOff,
+  FiBluetooth,
+  FiBold,
+  FiBook,
+  FiBookmark,
+  FiBookOpen,
+  FiBox,
+  FiBriefcase,
+  FiCalendar,
+  FiCamera,
+  FiCameraOff,
+  FiCast,
+  FiCheck,
+  FiCheckCircle,
+  FiCheckSquare,
+  FiChevronDown,
+  FiChevronLeft,
+  FiChevronRight,
+  FiChevronsDown,
+  FiChevronsLeft,
+  FiChevronsRight,
+  FiChevronsUp,
+  FiChevronUp,
+  FiChrome,
+  FiCircle,
+  FiClipboard,
+  FiClock,
+  FiCloud,
+  FiCloudDrizzle,
+  FiCloudLightning,
+  FiCloudOff,
+  FiCloudRain,
+  FiCloudSnow,
+  FiCode,
+  FiCodepen,
+  FiCodesandbox,
+  FiCoffee,
+  FiColumns,
+  FiCommand,
+  FiCompass,
+  FiCopy,
+  FiCornerDownLeft,
+  FiCornerDownRight,
+  FiCornerLeftDown,
+  FiCornerLeftUp,
+  FiCornerRightDown,
+  FiCornerRightUp,
+  FiCornerUpLeft,
+  FiCornerUpRight,
+  FiCpu,
+  FiCreditCard,
+  FiCrop,
+  FiCrosshair,
+  FiDatabase,
+  FiDelete,
+  FiDisc,
+  FiDivide,
+  FiDivideCircle,
+  FiDivideSquare,
+  FiDollarSign,
+  FiDownload,
+  FiDownloadCloud,
+  FiDribbble,
+  FiDroplet,
+  FiEdit,
+  FiEdit2,
+  FiEdit3,
+  FiExternalLink,
+  FiEye,
+  FiEyeOff,
+  FiFacebook,
+  FiFastForward,
+  FiFeather,
+  FiFigma,
+  FiFile,
+  FiFileMinus,
+  FiFilePlus,
+  FiFileText,
+  FiFilm,
+  FiFilter,
+  FiFlag,
+  FiFolder,
+  FiFolderMinus,
+  FiFolderPlus,
+  FiFramer,
+  FiFrown,
+  FiGift,
+  FiGitBranch,
+  FiGitCommit,
+  FiGithub,
+  FiGitlab,
+  FiGitMerge,
+  FiGitPullRequest,
+  FiGlobe,
+  FiGrid,
+  FiHardDrive,
+  FiHash,
+  FiHeadphones,
+  FiHeart,
+  FiHelpCircle,
+  FiHexagon,
+  FiHome,
+  FiImage,
+  FiInbox,
+  FiInfo,
+  FiInstagram,
+  FiItalic,
+  FiKey,
+  FiLayers,
+  FiLayout,
+  FiLifeBuoy,
+  FiLink,
+  FiLink2,
+  FiLinkedin,
+  FiList,
+  FiLoader,
+  FiLock,
+  FiLogIn,
+  FiLogOut,
+  FiMail,
+  FiMap,
+  FiMapPin,
+  FiMaximize,
+  FiMaximize2,
+  FiMeh,
+  FiMenu,
+  FiMessageCircle,
+  FiMessageSquare,
+  FiMic,
+  FiMicOff,
+  FiMinimize,
+  FiMinimize2,
+  FiMinus,
+  FiMinusCircle,
+  FiMinusSquare,
+  FiMonitor,
+  FiMoon,
+  FiMoreHorizontal,
+  FiMoreVertical,
+  FiMousePointer,
+  FiMove,
+  FiMusic,
+  FiNavigation,
+  FiNavigation2,
+  FiOctagon,
+  FiPackage,
+  FiPaperclip,
+  FiPause,
+  FiPauseCircle,
+  FiPenTool,
+  FiPercent,
+  FiPhone,
+  FiPhoneCall,
+  FiPhoneForwarded,
+  FiPhoneIncoming,
+  FiPhoneMissed,
+  FiPhoneOff,
+  FiPhoneOutgoing,
+  FiPieChart,
+  FiPlay,
+  FiPlayCircle,
+  FiPlus,
+  FiPlusCircle,
+  FiPlusSquare,
+  FiPocket,
+  FiPower,
+  FiPrinter,
+  FiRadio,
+  FiRefreshCcw,
+  FiRefreshCw,
+  FiRepeat,
+  FiRewind,
+  FiRotateCcw,
+  FiRotateCw,
+  FiRss,
+  FiSave,
+  FiScissors,
+  FiSearch,
+  FiSend,
+  FiServer,
+  FiSettings,
+  FiShare,
+  FiShare2,
+  FiShield,
+  FiShieldOff,
+  FiShoppingBag,
+  FiShoppingCart,
+  FiShuffle,
+  FiSidebar,
+  FiSkipBack,
+  FiSkipForward,
+  FiSlack,
+  FiSlash,
+  FiSliders,
+  FiSmartphone,
+  FiSmile,
+  FiSpeaker,
+  FiSquare,
+  FiStar,
+  FiStopCircle,
+  FiSun,
+  FiSunrise,
+  FiSunset,
+  FiTable,
+  FiTablet,
+  FiTag,
+  FiTarget,
+  FiTerminal,
+  FiThermometer,
+  FiThumbsDown,
+  FiThumbsUp,
+  FiToggleLeft,
+  FiToggleRight,
+  FiTool,
+  FiTrash,
+  FiTrash2,
+  FiTrello,
+  FiTrendingDown,
+  FiTrendingUp,
+  FiTriangle,
+  FiTruck,
+  FiTv,
+  FiTwitch,
+  FiTwitter,
+  FiType,
+  FiUmbrella,
+  FiUnderline,
+  FiUnlock,
+  FiUpload,
+  FiUploadCloud,
+  FiUser,
+  FiUserCheck,
+  FiUserMinus,
+  FiUserPlus,
+  FiUsers,
+  FiUserX,
+  FiVideo,
+  FiVideoOff,
+  FiVoicemail,
+  FiVolume,
+  FiVolume1,
+  FiVolume2,
+  FiVolumeX,
+  FiWatch,
+  FiWifi,
+  FiWifiOff,
+  FiWind,
+  FiX,
+  FiXCircle,
+  FiXOctagon,
+  FiXSquare,
+  FiYoutube,
+  FiZap,
+  FiZapOff,
+  FiZoomIn,
+  FiZoomOut,
+} as unknown as Record<string, IconData>;
+
+export { fiIconNames, type FiIconName } from "./names.ts";
