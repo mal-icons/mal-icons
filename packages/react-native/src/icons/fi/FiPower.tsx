c1e2fff98167818879f5760db23ca419fd4461fb
M Anas Latif <m.anaslatif.com@gmail.com>
2026-06-04 15:58:44 +0500

    feat(react-native): generate icon components (3/3)

---
+++ packages/react-native/src/icons/fi/FiPower.tsx
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.tsx";
+
+export const FiPower = createIcon("0 0 24 24", [["path",{"d":"M18.36 6.64a9 9 0 1 1-12.73 0"}],["line",{"x1":"12","y1":"2","x2":"12","y2":"12"}]], {"fill":"none","strokeWidth":2,"strokeLinecap":"round","strokeLinejoin":"round"});
---
+++ packages/react-native/src/icons/fi/FiVoicemail.tsx
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.tsx";
+
+export const FiVoicemail = createIcon("0 0 24 24", [["circle",{"cx":"5.5","cy":"11.5","r":"4.5"}],["circle",{"cx":"18.5","cy":"11.5","r":"4.5"}],["line",{"x1":"5.5","y1":"16","x2":"18.5","y2":"16"}]], {"fill":"none","strokeWidth":2,"strokeLinecap":"round","strokeLinejoin":"round"});
