61a281bc8465347d4d8b1a69ae6ac443e3f3c076
M Anas Latif <m.anaslatif.com@gmail.com>
2026-06-04 15:58:43 +0500

    feat(preact): generate icon components (1/3)

---
+++ packages/preact/src/icons/fi/FiArrowUpLeft.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiArrowUpLeft = createIcon("0 0 24 24", [["line",{"x1":"17","y1":"17","x2":"7","y2":"7"}],["polyline",{"points":"7 17 7 7 17 7"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
---
+++ packages/preact/src/icons/fi/FiCreditCard.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiCreditCard = createIcon("0 0 24 24", [["rect",{"x":"1","y":"4","width":"22","height":"16","rx":"2","ry":"2"}],["line",{"x1":"1","y1":"10","x2":"23","y2":"10"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
