1acf27edc1b5e97d0f74df45a6f7f7fc63fb6711
M Anas Latif <m.anaslatif.com@gmail.com>
2026-06-04 15:58:43 +0500

    feat(preact): generate icon components (2/3)

---
+++ packages/preact/src/icons/fi/FiMaximize2.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiMaximize2 = createIcon("0 0 24 24", [["polyline",{"points":"15 3 21 3 21 9"}],["polyline",{"points":"9 21 3 21 3 15"}],["line",{"x1":"21","y1":"3","x2":"14","y2":"10"}],["line",{"x1":"3","y1":"21","x2":"10","y2":"14"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
---
+++ packages/preact/src/icons/fi/FiPenTool.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiPenTool = createIcon("0 0 24 24", [["path",{"d":"M12 19l7-7 3 3-7 7-3-3z"}],["path",{"d":"M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"}],["path",{"d":"M2 2l7.59 7.59"}],["circle",{"cx":"11","cy":"11","r":"2"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
