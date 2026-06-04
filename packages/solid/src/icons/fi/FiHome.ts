6e0c3bd37c0c9c890ab3f4c676e5d10f4a7429ec
M Anas Latif <m.anaslatif.com@gmail.com>
2026-06-04 15:58:44 +0500

    feat(solid): generate icon components (2/3)

---
+++ packages/solid/src/icons/fi/FiHome.ts
@@ -0,0 +1,3 @@
+import { createIcon } from "../../create-icon.ts";
+
+export const FiHome = createIcon("0 0 24 24", [["path",{"d":"M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"}],["polyline",{"points":"9 22 9 12 15 12 15 22"}]], {"fill":"none","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"});
